3a16c1c3f737534d1faa6a75b395fe01e2bb25f8
Martin <m4rtin.t@gmail.com>
2016-10-01 00:59:16 -0400

    Quick validation fix for html when showing the error message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,10 +15,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        /**
+        *
+        * Validating API Key through calling
+        * http://www.xboxapi.com/v2/profile with given API key
+        *
+        */
+
         Validator::extend('correctapi', function($attribute, $value, $parameters, $validator) {
+            //Grab JSON data by calling API
             $json = call_API($value,'profile');
-            var_dump($json);
+
+            //Check if response include error_code
             if (isset($json['error_code'])){
+                //Place error message onto website
                 $validator->addReplacer('correctapi',  function ($message, $attribute, $rule, $parameters) use ($json) {
                     return str_replace([':error'], [$json['error_message']], $message);
                 });
@@ -26,10 +36,6 @@ class AppServiceProvider extends ServiceProvider
             } else {
                 return true;
             }
-        });
-
-        Validator::replacer('correctapi', function($message, $attribute, $rule, $parameters){
-            return str_replace(':error', 'THE ENTERED VALUE', $message);
         });
     }
 
