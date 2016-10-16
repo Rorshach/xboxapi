28d641892e7b5013394f9e2c0796f76b10c545be
Martin <m4rtin.t@gmail.com>
2016-10-15 20:58:51 -0700

    warning message on register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,9 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            <div class="alert alert-warning">
+                *Warning*  The registration process is pretty tedious. By the end, you will be able to send one message to 60 players. Choice is yours.
+            </div>
             <div class="panel panel-default">
                 <div class="panel-heading">Register</div>
                 <div class="panel-body">
